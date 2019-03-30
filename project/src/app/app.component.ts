280ffebdfab3dedca204bc15fc4f37dcf7533bdf
Aleksander Seryogin <a.siarohin@gmail.com>
2019-03-30 18:36:08 +0300

    feat: add film service

--- project/src/app/app.component.ts
+++ project/src/app/app.component.ts
@@ -1,42 +1,21 @@
-import { Component, OnInit, Input, AfterViewChecked, Output } from '@angular/core';
+import { Component, Optional } from '@angular/core';
+import { FilmService } from './film.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements AfterViewChecked {
+export class AppComponent {
   title = 'Movie';
-  value: string;
+  inputValue: string;
   inputFocusActive: boolean = false;
+  filmList: object[];
 
-  filmList: object[] = [{
-    imageURL: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg',
-    name: 'How to Train Your Dragon: The Hidden World',
-    actors: ['Jay Baruche', 'America Ferrera', 'F. Murray Abraham'],
-    detail: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium minima alias earum atque beatae aperiam, eligendi laboriosam quo vero velit mollitia neque exercitationem provident accusantium voluptas aspernatur dignissimos adipisci rerum molestiae fugiat fugit. Repellendus velit praesentium quod hic cum eaque aut incidunt nobis laboriosam enim repellat, odio et ipsam pariatur possimus omnis debitis corrupti reprehenderit deleniti ratione quo. Unde quia magni temporibus omnis quis.'
-  },
-  {
-    imageURL: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/AtsgWhDnHTq68L0lLsUrCnM7TjG.jpg',
-    name: 'Captain Marvel',
-    actors: ['Brie Larson', 'Samuel L. Jackson', 'Jude Law']
-  },
-  {
-    imageURL: 'https://image.tmdb.org/t/p/w600_and_h900_bestv2/nVN7Dt0Xr78gnJepRsRLaLYklbY.jpg',
-    name: 'Doom Patrol',
-    actors: ['Timothy Dalton', 'Matt Bomer', 'Brendan Fraser']
-  }];
-
-  constructor() {
-
+  constructor(private filmService: FilmService) {
   }
-
 
   onInit() {
-  }
-
-  ngAfterViewChecked() {
-
   }
 
   onInputFocus(event: Event): void {
@@ -47,7 +26,8 @@ export class AppComponent implements AfterViewChecked {
     this.inputFocusActive = false;
   }
 
-  getInputValue(inputValue: string): void {
-    this.value = inputValue;
+  onInputChange(value: string): void {
+    this.inputValue = value;
+    this.filmList = this.filmService.getData(value);
   }
 }
