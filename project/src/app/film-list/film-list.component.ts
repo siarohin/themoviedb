e3c73a3cd9ed55eadcb5b0a17d27a59eccbc34ac
Aleksander Seryogin <a.siarohin@gmail.com>
2019-03-30 14:10:22 +0300

    refactor: add @HostBinding to list-component

--- project/src/app/film-list/film-list.component.ts
+++ project/src/app/film-list/film-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding } from '@angular/core';
 
 @Component({
   selector: 'app-film-list',
@@ -6,6 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./film-list.component.scss']
 })
 export class FilmListComponent implements OnInit {
+  @HostBinding('class') className = 'main__film-list';
+
   @Input()
   films: object[];
 
