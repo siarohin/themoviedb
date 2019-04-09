8e917e5a97e7cf0f69d10cca1e01fca6c1656d35
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-04-09 15:56:16 +0300

    feat: add tests to FilmDetailComponent

--- project/src/app/components/film-detail/film-detail.component.spec.ts
+++ project/src/app/components/film-detail/film-detail.component.spec.ts
@@ -72,19 +72,77 @@ describe('FilmDetailComponent', () => {
     let title = debugElement.query(By.css('h1'));
     let div = debugElement.query(By.css('div'));
     let ul = debugElement.query(By.css('ul'));
+    let li = debugElement.query(By.css('li'));
     expect(article).toBeDefined();
     expect(title).toBeDefined();
     expect(div).toBeDefined();
     expect(ul).toBeDefined();
+    expect(li).toBeDefined();
 
     article = article.nativeElement;
     title = title.nativeElement;
     div = div.nativeElement;
     ul = ul.nativeElement;
+    li = li.nativeElement;
     expect(article.className).toContain('film__overview');
     expect(title.className).toContain('film__title');
     expect(div.className).toContain('film__image wrapper');
     expect(ul.className).toContain('actors-data actors');
+    expect(li.className).toContain('actors-item');
+  });
+
+  it('shouldnt add actors in template on empty actorList', () => {
+    component.film = {
+        actors: [],
+        id: 913516,
+        original_title: 'Marvel Film',
+        title: 'Marvel',
+        adult: false,
+        backdrop_path: 'img url',
+        genre_ids: [1, 2],
+        original_language: 'ru',
+        overview: 'Lorem ipsum',
+        popularity: 89,
+        poster_path: 'img url',
+        release_date: '11-20-19',
+        video: false,
+        vote_average: 18,
+        vote_count: 91,
+      };
+    fixture.detectChanges();
+    const li = debugElement.query(By.css('li'));
+    expect(li).toBeNull();
+  });
+
+  it('should add all films to template', () => {
+    component.film = {
+        actors: ['John', 'Doll'],
+        id: 913516,
+        original_title: 'Marvel Film',
+        title: 'Marvel',
+        adult: false,
+        backdrop_path: '/img.jpg',
+        genre_ids: [1, 2],
+        original_language: 'ru',
+        overview: 'Lorem ipsum',
+        popularity: 89,
+        poster_path: '/img.jpg',
+        release_date: '11-20-19',
+        video: false,
+        vote_average: 18,
+        vote_count: 91,
+      };
+    fixture.detectChanges();
+    const article = debugElement.query(By.css('article')).nativeElement;
+    const title = debugElement.query(By.css('h1')).nativeElement;
+    const img = debugElement.query(By.css('img')).nativeElement;
+    const li = debugElement.queryAll(By.css('li'));
+    expect(article.innerText).toBe('Lorem ipsum');
+    expect(title.innerText).toBe('Marvel Film');
+    expect(img.src).toBe('https://image.tmdb.org/t/p/w600_and_h900_bestv2/img.jpg');
+    expect(li[0].nativeElement.innerText).toBe('John');
+    expect(li[1].nativeElement.innerText).toBe('Doll');
+
   });
 
 });
