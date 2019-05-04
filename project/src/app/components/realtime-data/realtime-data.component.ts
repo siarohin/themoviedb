75755f515cffb34391da8f4281e8d38fbb5f010d
Aleksandr Seryogin <a.siarohin@gmail.com>
2019-05-04 16:03:53 +0300

    feat: add d3

--- project/src/app/components/realtime-data/realtime-data.component.ts
+++ project/src/app/components/realtime-data/realtime-data.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { D3Service, D3, Selection } from 'd3-ng2-service';
+
 import { GeneratorValueService } from '../../core/index';
 
 @Component({
@@ -7,14 +12,37 @@ import { GeneratorValueService } from '../../core/index';
     styleUrls: ['./realtime-data.component.css']
 })
 export class RealtimeDataComponent implements OnInit {
+    private d3: D3;
+    private parentNativeElement: any;
     private generatorValueService: GeneratorValueService;
-    public data: any;
 
-    constructor(generatorValueService: GeneratorValueService) {
+    public d3ParentElement: Selection<any, any, any, any>;
+
+    public randomValue: Observable<number>;
+
+    constructor(
+        generatorValueService: GeneratorValueService,
+        element: ElementRef,
+        d3Service: D3Service
+    ) {
+        this.d3 = d3Service.getD3();
+        this.parentNativeElement = element.nativeElement;
         this.generatorValueService = generatorValueService;
     }
 
     ngOnInit() {
-        this.data = this.generatorValueService.init();
+        this.randomValue = this.generatorValueService.init();
+
+        const d3 = this.d3;
+        if (this.parentNativeElement !== null) {
+            this.d3ParentElement = d3.select(this.parentNativeElement);
+        }
+
+        d3.select('.content')
+            .selectAll('p')
+            .data([4, 8, 15, 16, 23, 42])
+            .enter()
+            .append('p')
+            .text(d => d);
     }
 }
